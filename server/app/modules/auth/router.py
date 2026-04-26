15caa06f37c4b6311c321cbc86e5eab60b0aa948
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-04-26 18:15:42 +0300

    fix: existing account check

--- server/app/modules/auth/router.py
+++ server/app/modules/auth/router.py
@@ -3,17 +3,16 @@ from sqlalchemy.orm import Session
 from app.config.database import get_db
 from app.modules.users.models import User
 from app.modules.auth import schemas, utils
+from fastapi.security import OAuth2PasswordRequestForm 
 
 router = APIRouter()
 
 @router.post("/register", response_model=schemas.UserResponse)
 def register(user_in: schemas.UserCreate, db: Session = Depends(get_db)):
-    # 1. Check if user already exists
     user = db.query(User).filter(User.email == user_in.email).first()
     if user:
         raise HTTPException(status_code=400, detail="Email already registered")
     
-    # 2. Create new user object
     new_user = User(
         email=user_in.email,
         full_name=user_in.full_name,
@@ -21,25 +20,24 @@ def register(user_in: schemas.UserCreate, db: Session = Depends(get_db)):
         role=user_in.role
     )
     
-    # 3. Save to Neon
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
 
 @router.post("/login", response_model=schemas.Token)
-def login(user_in: schemas.UserLogin, db: Session = Depends(get_db)):
-    # 1. Find user by email
-    user = db.query(User).filter(User.email == user_in.email).first()
+def login(
+    db: Session = Depends(get_db), 
+    form_data: OAuth2PasswordRequestForm = Depends() 
+):
+    user = db.query(User).filter(User.email == form_data.username).first()
     
-    # 2. Verify password
-    if not user or not utils.verify_password(user_in.password, user.hashed_password):
+    if not user or not utils.verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, 
             detail="Invalid email or password"
         )
     
-    # 3. Create JWT Token
     access_token = utils.create_access_token(subject=user.id)
     
     return {
